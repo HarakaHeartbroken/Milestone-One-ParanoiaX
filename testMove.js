de9cb63502f4c67816a284ad02dbb554a0cc233d
Chris Foster <christophervfoster@gmail.com>
2021-12-10 22:52:55 -0800

    12/10 end commit, testbed map render successful, limited tileset deployed

--- testMove.js
+++ testMove.js
@@ -11,9 +11,9 @@
 function move(element) {
     element.style.position = 'fixed'
 
-    function moveToCoordinates(top, left) {
-        element.style.top = top + 'px'
+    function moveToCoordinates(left, bottom) {
         element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
     }
 
     function moveWithArrowKeys(left, bottom, callback){
--- testbed.js
+++ testbed.js
@@ -12,24 +12,78 @@
 //     ]
 //     mapRender(testmap)
 
+// 415
+// 263
+// 718
+
+
+var demoMap = [
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+    [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+    [3,0,0,0,0,0,0,0,0,0,4,1,1,5,0,0,0,0,0,0,0,0,0,2],
+    [3,0,0,0,0,0,0,0,0,0,2,6,6,3,0,0,0,0,0,0,0,0,0,2],
+    [3,0,0,0,0,0,0,0,0,0,2,6,6,3,0,0,0,0,0,0,0,0,0,2],
+    [3,0,0,0,0,0,0,4,1,1,8,6,6,7,1,1,5,0,0,0,0,0,0,2],
+    [3,0,0,0,0,0,0,2,6,6,6,6,6,6,6,6,3,0,0,0,0,0,0,2],
+    [3,0,0,0,0,0,0,2,6,6,6,6,6,6,6,6,3,0,0,0,0,0,0,2],
+    [3,0,0,0,0,0,0,7,1,1,5,6,6,4,1,1,8,0,0,0,0,0,0,2],
+    [3,0,0,0,0,0,0,0,0,0,2,6,6,3,0,0,0,0,0,0,0,0,0,2],
+    [3,0,0,0,0,0,0,0,0,0,2,6,6,3,0,0,0,0,0,0,0,0,0,2],
+    [3,0,0,0,0,0,0,0,0,0,7,1,1,8,0,0,0,0,0,0,0,0,0,2],
+    [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+    [3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+  ];
+
+
+
+
+
+
 
 var testmap = [
     [1, 2, 3], 
-    [1, 0, 1], 
+    [1, 0, 0], 
     [1, 1, 1]
 ];
-
-// map.forEach(element => console.log(element));
-
-
 function test(map){
     for(i = 0; i<map.length; i++){
         for(j = 0; j<map[i].length; j++){
-            if(map[i][j] > 1)
-            console.log(map[i][j])
+            if(map[i][j] === 0){
+                move(newImage('assets/bgTile.png')).to(16*i, 16*j)
+            }
+            else if(map[i][j] === 1){
+                move(newImage('assets/wall1.png')).to(16*i, 16*j)
+            }
+            else if(map[i][j] === 2){
+                move(newImage('assets/wall6.png')).to(16*i, 16*j)
+            }
+            else if(map[i][j] === 3){
+                move(newImage('assets/wall6.png')).to(16*i, 16*j)
+            }
+            else if(map[i][j] === 4){
+                move(newImage('assets/wall4.png')).to(16*i, 16*j)
+            }
+            else if(map[i][j] === 5){
+                move(newImage('assets/wall5.png')).to(16*i, 16*j)
+            }
+            else if(map[i][j] === 6){
+                move(newImage('assets/intWall.png')).to(16*i, 16*j)
+            }
+            else if(map[i][j] === 7){
+                move(newImage('assets/wall7.png')).to(16*i, 16*j)
+            }
+            else if(map[i][j] === 8){
+                move(newImage('assets/wall8.png')).to(16*i, 16*j)
+            }
+            else if(map[i][j] === 9){
+                move(newImage('assets/wall9.png')).to(16*i, 16*j)
+            }
         }
     }
 }
-test(testmap)
-// expected output 2, 3
+const pc = newPlayableCharacter(100, 110)
+test(demoMap)
+// expected output wall leftWall rightWall wall background wall wall wall wall
 // console.log(map[])
