9439af577d095edf2078caf3945d45a6a361f94c
Chris Foster <christophervfoster@gmail.com>
2021-12-15 00:32:48 -0800

    Added further storyline/interaction webs

--- story.js
+++ story.js
@@ -4,10 +4,25 @@
 // initial- RPS game, with extra command for "exit" and more explicit command list options
 // on exit, load page- render(map), pc = new Player, etc
 
-// Player starts locked in a cell, theres a console, a red beam fence, and some interactibles
+// Player starts locked in a hallway, theres a door, a console, a red beam fence, and some interactibles
 // console allows you to ask for several things
+// door is just a prompt that "Retreating is TREASON, Citizen!"
+// and kills you
+// For purposes of Demo, "you can't die until you've completed your mission, you'll be
+// executed for every clone lost after this point until you finish your mission."
+// Just count negative clones left as a joke, maybe a full win if you have positive lives left
+// Real game, you have 6 clones to begin with, can gain credits during/for each mission, 
+// can use credits to buy clones and random stuff/meta game
+
+
+
 // asking to turn the fence off is above your clearance and knowing that is above your clearance
-// you can ask for food, mercy, etc
+// you can ask for food, mercy (killing), etc, basically all unhelpful and occasionally...
+// lethal stuff
+// all dropped items can be clicked on or ran over to add to inventory
+// clicking on an item in inventory (stretch keyboard/arrow control) offers USE options
+// LOOK, USE, DROP, EAT, whatever
+// Think old Sierra Adventure games
 
 // Solution 1- ask for Limon-Lem Bouncy Bubble Beverage, which is YELLOW
 // pour/rub/use BBB on self, coloring self YELLOW
@@ -21,10 +36,9 @@
 // Solution 3- Check the loose panel to the left side of the wall, leave the cell through 
 // the hole. 
 
+// False solution, rub oil on self to turn suit INFRARED, laser is RED clearance and 
+// Friend Computer will happily remind them of that
 
-// either way, there's an interactible door at the far end of the map, leadsa to the win screen
 
-
-
-// Eventually lead to "You needed to get the RPS module out of the console, you'll know
-// which console, it's in the room with the broken fence."
+// any way, there's an interactible door at the far end of the map, leads to the win screen
+// in the demo, leads to another map/story/challenge in the full game
