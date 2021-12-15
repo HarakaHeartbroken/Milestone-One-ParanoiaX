cf70b4fc168f1119a09d9af149af852523c82983
Chris Foster <christophervfoster@gmail.com>
2021-12-14 17:05:43 -0800

    Initial game script/storyline added

---
+++ story.js
@@ -0,0 +1,30 @@
+// placeholder for story web/scripting
+
+
+// initial- RPS game, with extra command for "exit" and more explicit command list options
+// on exit, load page- render(map), pc = new Player, etc
+
+// Player starts locked in a cell, theres a console, a red beam fence, and some interactibles
+// console allows you to ask for several things
+// asking to turn the fence off is above your clearance and knowing that is above your clearance
+// you can ask for food, mercy, etc
+
+// Solution 1- ask for Limon-Lem Bouncy Bubble Beverage, which is YELLOW
+// pour/rub/use BBB on self, coloring self YELLOW
+// walk through the fence
+
+// Solution 2- ask for food
+// wait until the Infrared/robot delivery person comes, they'll turn off the field, walk in,
+// drop FOOD, leave, turn the field back on. Just walk out when the fence is off, no one
+// cares.
+
+// Solution 3- Check the loose panel to the left side of the wall, leave the cell through 
+// the hole. 
+
+
+// either way, there's an interactible door at the far end of the map, leadsa to the win screen
+
+
+
+// Eventually lead to "You needed to get the RPS module out of the console, you'll know
+// which console, it's in the room with the broken fence."
