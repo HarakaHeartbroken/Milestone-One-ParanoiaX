c0beea63e15c001c3b546c68350172f8d5a5cea6
Chris Foster <christophervfoster@gmail.com>
2021-12-18 01:44:01 -0800

    map rendering functions and majority of crafty component defining work done. Collision detection set up, but not successfully implemented. Current build version fully annotated and assembled.

--- ParanoiaX/ParanoiaX.js
+++ ParanoiaX/ParanoiaX.js
@@ -1,5 +1,9 @@
 Crafty.init(600, 400,);
 
+
+
+// A series of map arrays that represent rendering layers of the game screen.
+
 // Map Key:
 // 00- empty Tile
 // 01- horizontal wall
@@ -15,6 +19,28 @@ Crafty.init(600, 400,);
 // 11- computer terminal
 // 12- green goo
 // 13-
+
+
+var backMap = [
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00]
+  ];
+
+
 var demoMap = [
     [03,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,04],
     [02,12,00,00,00,00,00,00,00,00,02,00,00,02,00,00,00,00,00,00,00,00,00,02],
@@ -27,13 +53,34 @@ var demoMap = [
     [02,00,00,00,00,00,00,02,00,00,00,00,00,00,00,00,02,00,00,00,00,00,00,02],
     [02,00,00,00,00,00,00,06,01,01,04,00,00,03,01,01,07,00,00,00,00,00,00,02],
     [02,00,00,00,00,00,00,00,11,00,02,00,00,02,00,00,00,00,00,00,00,00,00,02],
-    [02,00,00,00,00,00,00,00,00,00,02,00,00,03,00,00,00,00,00,00,00,00,00,02],
+    [02,00,00,00,00,00,00,00,00,00,02,00,00,02,00,00,00,00,00,00,00,00,00,02],
     [02,00,00,00,00,00,00,00,00,00,08,09,09,10,00,00,00,00,00,00,00,00,00,02],
     [02,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,02],
     [02,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,02],
     [06,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,07]
   ];
 
+  var decMap = [
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],
+    [00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00]
+  ];
+
+
+// a series of asset definitions using sprite tilesheet terminology
 var map_assets = {
     "sprites": {
         "tileset.png": {
@@ -84,141 +131,211 @@ var green_assets = {
     }
 };
    
-Crafty.c("floor", {
-    init: function() {
-        this.requires('Tile, 2D, Canvas')
-      },
-})
-Crafty.c("horWall", {
-    init: function() {
-        this.requires('Tile, 2D, Canvas, Wall')
-      },
-})
-Crafty.c("vertWall", {
-    init: function() {
-        this.requires('Tile, 2D, Canvas, Wall')
-      },
-})
-Crafty.c("topLeft", {
-    init: function() {
-        this.requires('Tile, 2D, Canvas, Wall')
-      },
-})
-Crafty.c("topRight", {
-    init: function() {
-        this.requires('Tile, 2D, Canvas, Wall')
-      },
-})
-Crafty.c("interiorWall", {
-    init: function() {
-        this.requires('Tile, 2D, Canvas, Wall')
-      },
-})
-Crafty.c("bottomLeft", {
-    init: function() {
-        this.requires('Tile, 2D, Canvas, Wall')
-      },
-})
-Crafty.c("bottomRight", {
-    init: function() {
-        this.requires('Tile, 2D, Canvas, Wall')
-      },
-})
-Crafty.c("leftLaser", {
-    init: function() {
-        this.requires('Tile, 2D, Canvas, Wall')
-      },
-})
-Crafty.c("horlaserWall", {
-    init: function() {
-        this.requires('Tile, 2D, Canvas, Hazard')
-      },
-})
-Crafty.c("rightLaser", {
-    init: function() {
-        this.requires('Tile, 2D, Canvas, Wall')
-      },
-})
-Crafty.c("computerTerminal", {
-    init: function() {
-        this.requires('Tile, 2D, Canvas, Interactible')
-      },
-})
-Crafty.c("goo", {
-    init: function() {
-        this.requires('Tile, 2D, Canvas, Interactible')
-      },
-})
+var computer_assets = {
+    "sprites": {
+        "computer.png": {
+            tile: 16,
+            tileh: 16,
+            map: {
+                    termOn: [0, 0],
+                    termOff: [1, 0]
+                }
+        }
+    }
+};
+
+var environmental_assets = {
+    "sprites": {
+        "doodads.png": {
+            tile: 16,
+            tileh: 16,
+            map: {
+                    gooPour: [0, 0],
+                    gooSpat1: [5, 3],
+                    radSymbol: [3, 1],
+                    blood1: [2, 0],
+                    blood2: [3, 0],
+                    blood3: [4, 0],
+                    blood4: [5, 0],
+                    hazStripe: [0, 3],
+                    extinguisher: [2, 3],
+                    hazSymbol: [3, 1],
+                    readOut: [5, 1]
+                }
+        }
+    }
+};
+
+var laser_assets = {
+    "sprites": {
+        "lasers.png": {
+            tile: 16,
+            tileh: 16,
+            map: {
+                    rightLaser: [9, 5],
+                    leftLaser: [9, 6],
+                    redLaser: [1, 0],
+                }
+        }
+    }
+};
+
+
+// Instructs the canvas to load all of the defined assets for use
+Crafty.load(red_assets);
+Crafty.load(green_assets);
+Crafty.load(map_assets);
+Crafty.load(computer_assets);
+Crafty.load(environmental_assets);
+Crafty.load(laser_assets);
 
 
 
 
 
 
+// a series of component definitions (Essentially classes)
+//TODO: Create "Tile" component, then subdivide that into "Wall" etc to reduce redundant code
+Crafty.c("floorTile", {
+    init: function() {
+        this.requires('Tile, 2D, Canvas, floor')
+      },
+})
+Crafty.c("horWallTile", {
+    init: function() {
+        this.requires('Tile, 2D, Canvas, Wall, horWall')
+      },
+})
+Crafty.c("vertWallTile", {
+    init: function() {
+        this.requires('Tile, 2D, Canvas, Wall, vertWall')
+      },
+})
+Crafty.c("topLeftTile", {
+    init: function() {
+        this.requires('Tile, 2D, Canvas, Wall, topLeft')
+      },
+})
+Crafty.c("topRightTile", {
+    init: function() {
+        this.requires('Tile, 2D, Canvas, Wall, topRight')
+      },
+})
+Crafty.c("bottomLeftTile", {
+    init: function() {
+        this.requires('Tile, 2D, Canvas, Wall, bottomLeft')
+      },
+})
+Crafty.c("bottomRightTile", {
+    init: function() {
+        this.requires('Tile, 2D, Canvas, Wall, bottomRight')
+      },
+})
+Crafty.c("leftLaserTile", {
+    init: function() {
+        this.requires('Tile, 2D, Canvas, , leftLaser, Wall')
+      },
+})
+Crafty.c("redLaserTile", {
+    init: function() {
+        this.requires('Tile, 2D, Canvas, redLaser, Hazard')
+      },
+})
+Crafty.c("rightLaserTile", {
+    init: function() {
+        this.requires('Tile, 2D, Canvas, rightLaser, Wall')
+      },
+})
+Crafty.c("computerTerminalTile", {
+    init: function() {
+        this.requires('Tile, 2D, Canvas, , termOn, Interactible')
+      },
+})
+Crafty.c("gooTile", {
+    init: function() {
+        this.requires('Tile, 2D, Canvas, gooPour, Interactible')
+      },
+});
 
-  Crafty.load(red_assets);
-  Crafty.load(green_assets);
-  Crafty.load(map_assets);
-
-// function mapRender(map){
-//     for(i = 0; i<map.length; i++){
-//         for(j = 0; j<map[i].length; j++){
-//             if(map[i][j] === 0){
-//                 Crafty.e("floor").at(i, j)
-//             }
-//             else if(map[i][j] === 1){
-//                 Crafty.e("horWall").at(i, j)
-//             }
-//             else if(map[i][j] === 2){
-//                 Crafty.e("vertWall").at(i, j)
-//             }
-//             else if(map[i][j] === 3){
-//                 Crafty.e("topLeft").at(i, j)
-//             }
-//             else if(map[i][j] === 4){
-//                 Crafty.e("topRight").at(i, j)
-//             }
-//             else if(map[i][j] === 5){
-//                 Crafty.e("interiorWall").at(i, j)
-//             }
-//             else if(map[i][j] === 6){
-//                 Crafty.e("bottomLeft").at(i, j)
-//             }
-//             else if(map[i][j] === 7){
-//                 Crafty.e("bottomRight").at(i, j)
-//             }
-//             else if(map[i][j] === 8){
-//                 Crafty.e("leftLaser").at(i, j)
-//             }
-//             else if(map[i][j] === 9){
-//                 Crafty.e("horlaserWall").at(i, j)
-//             }
-//             else if(map[i][j] === 10){
-//                 Crafty.e("rightLaser").at(i, j)
-//             }
-//             else if(map[i][j] === 11){
-//                 Crafty.e("computerTerminal").at(i, j)
-//             }
-//             else if(map[i][j] === 12){
-//                 Crafty.e("goo").at(i, j)
-//             }
-
-//         }
-//     } 
-// }
-  
-// mapRender(demoMap)
 
 
+// takes a given array map and renders it according to the map key at the z-index passed
+function mapRender(map, z){
+    for(i = 0; i<map.length; i++){
+        for(j = 0; j<map[i].length; j++){
+            if(map[i][j] === 00){
+                Crafty.e("floorTile")
+                .attr({x: (16*j), y: (16*i), z: z, w: 16, h: 16})
+            }
+            else if(map[i][j] === 01){
+                Crafty.e("horWallTile")
+                .attr({x: (16*j), y: (16*i), z: z, w: 16, h: 16})
+            }
+            else if(map[i][j] === 02){
+                Crafty.e("vertWallTile")
+                .attr({x: (16*j), y: (16*i), z: z, w: 16, h: 16})
+            }
+            else if(map[i][j] === 03){
+                Crafty.e("topLeftTile")
+                .attr({x: (16*j), y: (16*i), z: z, w: 16, h: 16})
+            }
+            else if(map[i][j] === 04){
+                Crafty.e("topRightTile")
+                .attr({x: (16*j), y: (16*i), z: z, w: 16, h: 16})
+            }
+            else if(map[i][j] === 05){
+                Crafty.e("interiorWallTile")
+                .attr({x: (16*j), y: (16*i), z: z, w: 16, h: 16})
+            }
+            else if(map[i][j] === 06){
+                Crafty.e("bottomLeftTile")
+                .attr({x: (16*j), y: (16*i), z: z, w: 16, h: 16})
+            }
+            else if(map[i][j] === 07){
+                Crafty.e("bottomRightTile")
+                .attr({x: (16*j), y: (16*i), z: z, w: 16, h: 16})
+            }
+            else if(map[i][j] === 08){
+                Crafty.e("leftLaserTile")
+                .attr({x: (16*j), y: (16*i), z: z, w: 16, h: 16})
+            }
+            else if(map[i][j] === 09){
+                Crafty.e("redLaserTile")
+                .attr({x: (16*j), y: (16*i), z: z, w: 16, h: 16})
+            }
+            else if(map[i][j] === 10){
+                Crafty.e("rightLaserTile")
+                .attr({x: (16*j), y: (16*i), z: z, w: 16, h: 16})
+            }
+            else if(map[i][j] === 11){
+                Crafty.e("computerTerminalTile")
+                .attr({x: (16*j), y: (16*i), z: z, w: 16, h: 16})
+            }
+            else if(map[i][j] === 12){
+                Crafty.e("gooTile")
+                .attr({x: (16*j), y: (16*i), z: z, w: 16, h: 16})
+            }
+
+        }
+    } 
+}
+
+// instructs the canvas to render the background layer and map layer of the game screen
+mapRender(demoMap, 1)
+mapRender(backMap, 0)
+
+// initializes the player character as a controllable rectangle with tags
   var player = Crafty.e("2D, Canvas, redstatic, SpriteAnimation, Fourway, Player")
   .attr({x: 50, y: 50, z: 2, w: 16, h: 16})
   .fourway(100)
-
+  
+// currently unused definition for a similiar character in green, for story use
 //   var greenPlayer = Crafty.e("2D, Canvas, greenstatic, SpriteAnimation, Fourway, Player")
 //   .attr({x: 70, y: 70, z: 2, w: 16, h: 16})
 //   .fourway(100)
 
-
+// a series of reel definitions to create gifs from the same sprite sheet, passing a name
+// and number of milliseconds for the animation to run in
 player.reel("redwalkDown", 1000, [
     [0,0],
     [1,0],
@@ -244,6 +361,8 @@ player.reel("redwalkRight", 1000, [
     [3,3]
 ])
 
+// handles directional input and animates the appropriate reel
+//TODO: add in a stop point to return the character to rest after keys are released
 player.bind("KeyDown", function(evt){
       if(evt.key === Crafty.keys.UP_ARROW){
           player.animate("redwalkUp", -1)
@@ -259,55 +378,49 @@ player.bind("KeyDown", function(evt){
       }
   })
 
-greenPlayer.reel("greenwalkDown", 1000, [
-    [0,0],
-    [1,0],
-    [2,0],
-    [3,0],
-])
-greenPlayer.reel("greenwalkUp", 1000, [
-    [0,1],
-    [1,1],
-    [2,1],
-    [3,1]
-])
-greenPlayer.reel("greenwalkLeft", 1000, [
-    [0,2],
-    [1,2],
-    [2,2],
-    [3,2]
-])
-greenPlayer.reel("greenwalkRight", 1000, [
-    [0,3],
-    [1,3],
-    [2,3],
-    [3,3]
-])
 
-greenPlayer.bind("KeyDown", function(evt){
-      if(evt.key === Crafty.keys.UP_ARROW){
-        greenPlayer.animate("greenwalkUp", -1)
-      }
-      if(evt.key === Crafty.keys.DOWN_ARROW){
-        greenPlayer.animate("greenwalkDown", -1)
-      }
-      if(evt.key === Crafty.keys.LEFT_ARROW){
-        greenPlayer.animate("greenwalkLeft", -1)
-      }
-      if(evt.key === Crafty.keys.RIGHT_ARROW){
-        greenPlayer.animate("greenwalkRight", -1)
-      }
-  })
-// player.bind(onHit('Wall'), function(){
-//     player.x = 
-//     player.y = 
-// })
+  // currently unused reel/animation process for the green version of the player character
+// greenPlayer.reel("greenwalkDown", 1000, [
+//     [0,0],
+//     [1,0],
+//     [2,0],
+//     [3,0],
+// ])
+// greenPlayer.reel("greenwalkUp", 1000, [
+//     [0,1],
+//     [1,1],
+//     [2,1],
+//     [3,1]
+// ])
+// greenPlayer.reel("greenwalkLeft", 1000, [
+//     [0,2],
+//     [1,2],
+//     [2,2],
+//     [3,2]
+// ])
+// greenPlayer.reel("greenwalkRight", 1000, [
+//     [0,3],
+//     [1,3],
+//     [2,3],
+//     [3,3]
+// ])
+
+// greenPlayer.bind("KeyDown", function(evt){
+//       if(evt.key === Crafty.keys.UP_ARROW){
+//         greenPlayer.animate("greenwalkUp", -1)
+//       }
+//       if(evt.key === Crafty.keys.DOWN_ARROW){
+//         greenPlayer.animate("greenwalkDown", -1)
+//       }
+//       if(evt.key === Crafty.keys.LEFT_ARROW){
+//         greenPlayer.animate("greenwalkLeft", -1)
+//       }
+//       if(evt.key === Crafty.keys.RIGHT_ARROW){
+//         greenPlayer.animate("greenwalkRight", -1)
+//       }
+//   })
 
 
 
 
-// player.checkHits("wall")
-//     .bind("HitOn", function(hitData){
-//         this.x = evt._x;
-//         this.y = evt._y;
-//     })
+
